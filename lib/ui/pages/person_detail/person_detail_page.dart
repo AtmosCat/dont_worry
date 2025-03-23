107051e78ef4e970bdd2b3de3eaefbe96fb988fa
AtmosCat <dnflro22@gmail.com>
2025-03-23 15:08:30 +0900

    impl(U-1LR-011): PersonDetailPage 대출 리스트 높은 가격순 정렬 기능 구현 완료, 업데이트순 구현 필요

--- lib/ui/pages/person_detail/person_detail_page.dart
+++ lib/ui/pages/person_detail/person_detail_page.dart
@@ -9,35 +9,73 @@ import 'package:dont_worry/utils/enum.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PersonDetailPage extends StatelessWidget {
+class PersonDetailPage extends StatefulWidget {
   final bool isLending;
   final Person person;
   const PersonDetailPage(
       {required this.isLending, required this.person, super.key});
 
-  // PersonDetailPage UI
+  @override
+  State<PersonDetailPage> createState() => _PersonDetailPageState();
+}
+
+class _PersonDetailPageState extends State<PersonDetailPage> {
+  bool? isSortedByUpdate;
+
+  @override
+  void initState() {
+    super.initState();
+    isSortedByUpdate = true;
+  }
+
+  void onSortOptionSelected(String value) {
+    setState(() {
+      isSortedByUpdate = value == '업데이트 순';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton:
-          CreateLoanFloatingActionButton(isLending: isLending, person: person),
+      floatingActionButton: CreateLoanFloatingActionButton(
+          isLending: widget.isLending, person: widget.person),
       // #1. 상단 앱바
       appBar: DetailAppBar(
-        isLending: isLending,
+        isLending: widget.isLending,
         unitType: UnitType.person,
-        person: person,
+        person: widget.person,
       ),
       body: ListView(
         children: [
           // #2. 헤더
-          PersonDetailHeader(personId: person.personId, isLending: isLending),
+          PersonDetailHeader(
+              personId: widget.person.personId, isLending: widget.isLending),
           Consumer(builder: (context, ref, child) {
             var stateLoans = ref.watch(appViewModelProvider.select((state) =>
                 state.loans.where((loan) =>
-                    loan.personId == person.personId &&
-                    loan.isLending == isLending)));
-            var inPaidList = stateLoans.where((loan) => !loan.isPaidOff);
-            var paidOffList = stateLoans.where((loan) => loan.isPaidOff);
+                    loan.personId == widget.person.personId &&
+                    loan.isLending == widget.isLending)));
+            var inPaidList = stateLoans.where((loan) => !loan.isPaidOff).toList();
+            var paidOffList = stateLoans.where((loan) => loan.isPaidOff).toList();
+
+            // 업데이트 순 정렬
+            if (isSortedByUpdate ?? true) {
+              inPaidList.sort((b, a) => a.updatedAt.compareTo(b.updatedAt));
+              paidOffList.sort((b, a) => a.updatedAt.compareTo(b.updatedAt));
+            }
+
+            // 높은 가격 순 정렬 (대출 금액 기준)
+            else if (widget.isLending) {
+              inPaidList.sort((b, a) =>
+                  a.remainingAmount.compareTo(b.remainingAmount));
+              paidOffList.sort(
+                  (b, a) => a.remainingAmount.compareTo(b.remainingAmount));
+            } else {
+              inPaidList.sort((b, a) =>
+                  a.remainingAmount.compareTo(b.remainingAmount));
+              paidOffList.sort((b, a) =>
+                  a.remainingAmount.compareTo(b.remainingAmount));
+            }
 
             return Visibility(
                 visible: stateLoans.isNotEmpty,
@@ -50,9 +88,12 @@ class PersonDetailPage extends StatelessWidget {
                             SizedBox(height: 10),
                             // #3-1. 미상환 대출 List
                             ListHeader(
-                                isLending: isLending, unitType: UnitType.loan),
+                              isLending: widget.isLending,
+                              unitType: UnitType.loan,
+                              onSortOptionSelected: onSortOptionSelected,
+                            ),
                             loanList(
-                              isLending: isLending,
+                              isLending: widget.isLending,
                               loansState: inPaidList.toList(),
                             ),
                           ],
@@ -63,9 +104,12 @@ class PersonDetailPage extends StatelessWidget {
                           children: [
                             SizedBox(height: 10),
                             // #3-2. 상환완료 대출 List
-                            ListHeader(unitType: UnitType.loan),
+                            ListHeader(
+                              unitType: UnitType.loan,
+                              onSortOptionSelected: onSortOptionSelected,
+                            ),
                             loanList(
-                              isLending: isLending,
+                              isLending: widget.isLending,
                               loansState: paidOffList.toList(),
                             ),
                           ],
